2c15d50651d3ecee4ce97a1ae7e598b06fdb348a
Richard Asp <richard@richardasp.com>
2018-04-11 14:18:13 -0500

    round2 changes

--- show_status.sh
+++ show_status.sh
@@ -2,6 +2,8 @@
 
 if [[ -e $1 ]] && [[  $1 != ''  ]]; then 
 	TARGET=$1
+        BRANCH=$(git rev-parse --abbrev-ref HEAD)
+        echo "Current Branch:  $BRANCH"
 	echo “HEAD:                $(git cat-file -p HEAD:$TARGET)”
 	echo “Index:               $(git cat-file -p :$TARGET)”
 	echo “Working Directory:    $(cat $TARGET)”
